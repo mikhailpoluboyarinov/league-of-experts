ad99933aab5a95790678858ff5b0e1cf2aa692f2
mikhailpoluboyarinov <mikhailpoluboyarinov@yandex.ru>
2024-05-25 19:57:58 +0300

    Убрал админку в другой проект

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,26 +4,8 @@ import { ScoresTableGroupPage } from "../ScoresTableGroupPage/ScoresTableGroupPa
 import { ScoresTablePlayoffPage } from "../ScoresTablePlayoffPage/ScoresTablePlayoffPage";
 import { NotFoundPage } from "../NotFoundPage/NotFoundPage";
 import { GameRulesPage } from "../GameRulesPage/GameRulesPage";
-import { LoginAdmin } from "../../components/LoginAdmin/LoginAdmin";
-import { MatchesPage } from "../MatchesPage/MatchesPage";
-import { CountriesPage } from "../CountriesPage/CountriesPage";
-import { PredictionsPage } from "../PredictionsPage/PredictionsPage";
-import { MiscPage } from "../MiscPage/MiscPage";
-import { AdminLayoutPage } from "../AdminLayoutPage/AdminLayoutPage";
-import { ResultsPage } from "../ResultsPage/ResultsPage";
-import { UsersPage } from "../UsersPage/UsersPage";
 
 export const Main = () => {
-  const handleLogin = ({
-    login,
-    password,
-  }: {
-    login: string;
-    password: string;
-  }) => {
-    console.log("login", login, password);
-  };
-
   return (
     <Routes>
       <Route path="/" element={<ScoresTableTotalPage />} />
@@ -31,18 +13,6 @@ export const Main = () => {
       <Route path="/playoff" element={<ScoresTablePlayoffPage />} />
       <Route path="/faq" element={<GameRulesPage />} />
       <Route path="*" element={<NotFoundPage />} />
-      <Route
-        path="/login/admin"
-        element={<LoginAdmin onLogin={handleLogin} />}
-      />
-      <Route path="/admin" element={<AdminLayoutPage />}>
-        <Route path="countries" element={<CountriesPage />} />
-        <Route path="matches" element={<MatchesPage />} />
-        <Route path="misc" element={<MiscPage />} />
-        <Route path="predictions" element={<PredictionsPage />} />
-        <Route path="results" element={<ResultsPage />} />
-        <Route path="users" element={<UsersPage />} />
-      </Route>
     </Routes>
   );
 };
