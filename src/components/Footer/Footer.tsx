9049e88f7f4681e1f558ae28ca0d8c3dc195f819
mikhailpoluboyarinov <mikhailpoluboyarinov@yandex.ru>
2024-07-04 16:04:55 +0300

    add ya music to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,18 @@
-import { AppBar, Box, Link, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Link,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import { CUSTOM_COLORS } from "../../styles/colors";
 import { Telegram } from "@mui/icons-material";
+import { ReactComponent as YaMusic } from "../../images/icon_YaMusic.svg";
 
 export const Footer = () => {
+  const isSmallScreen = useMediaQuery("(max-width: 650px)");
+
   return (
     <AppBar
       position="static"
@@ -16,16 +26,24 @@ export const Footer = () => {
         <Box sx={{ flexGrow: 1 }} />
         <Box sx={{ display: "flex", alignItems: "center" }}>
           <Typography variant="caption" style={{ marginRight: "10px" }}>
-            Новости футбола:
+            Новости футбола и музыка:
           </Typography>
           <Link
             target="_blank"
             href="https://t.me/morefoot"
             underline="none"
             color="white"
-            style={{ marginRight: "60px" }}
+            style={{ marginRight: "10px" }}
           >
             <Telegram />
+          </Link>
+          <Link
+            target="_blank"
+            href="https://music.yandex.ru/users/sir.alex.dee/playlists/1010"
+            underline="none"
+            style={{ marginRight: isSmallScreen ? `15px` : `60px` }}
+          >
+            <YaMusic style={{ width: "24px", height: "24px" }} />
           </Link>
         </Box>
       </Toolbar>
