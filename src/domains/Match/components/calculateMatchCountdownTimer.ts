f36c39cfa2ed378a42754cc34e4c4afe4666aed3
mikhailpoluboyarinov <mikhailpoluboyarinov@yandex.ru>
2024-07-05 14:17:49 +0300

    add timer for upcommingMatchCard

---
+++ src/domains/Match/components/calculateMatchCountdownTimer.ts
@@ -0,0 +1,24 @@
+// Ф-я для обратного отсчета до матча, отображается если остается меньше одного дня
+export const calculateMatchCountdownTimer = (startTimeMatch: number) => {
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+
+  let timeDifference = startTimeMatch - currentTimestamp;
+
+  //21600 = 6 часов
+  if (timeDifference > 21600 || timeDifference <= 0) {
+    return null;
+  }
+
+  const days = Math.floor(timeDifference / (24 * 3600));
+  timeDifference %= 24 * 3600;
+  const hours = Math.floor(timeDifference / 3600);
+  timeDifference %= 3600;
+  const minutes = Math.floor(timeDifference / 60);
+  const seconds = timeDifference % 60;
+
+  if (days > 0) {
+    return null;
+  } else {
+    return `${hours} : ${minutes} : ${seconds}`;
+  }
+};
